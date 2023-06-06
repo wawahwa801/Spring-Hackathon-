e8f816e8b340dd1c1141f688cb85c64159bb4b2f
Jonas Huang <jonaszchuang@gmail.com>
2023-06-05 21:57:25 -0600

    unity

---
+++ Hackathon/Assets/movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public float moveSpeed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Spring-Hackathon-
+version https://git-lfs.github.com/spec/v1
+oid sha256:410371af78ffa79f6d8c9790c240954a49090aa276b63ff30b2162552d57d1d0
+size 19
